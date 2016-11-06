c02abc1730d4b5f819e20d6fce3bbad4655c85e7
Andrew Drozdov <andrew@mrdrozdov.com>
2016-11-06 13:39:10 -0500

    Remove more Theano/C++ code and doc references.

--- python/spinn/util/__init__.py
+++ python/spinn/util/__init__.py
@@ -1,15 +1,7 @@
 from collections import namedtuple
 
-from theano.sandbox.cuda import cuda_available
 
-# Only import custom CUDA ops if we on a CUDA-enabled host.
-if cuda_available:
-    from spinn.util.cuda import *
-
-from spinn.util.theano_internal import *
 from spinn.util.data import *
-from spinn.util.blocks import *
-from spinn.util.variable_store import VariableStore
 
 
 ModelSpec_ = namedtuple("ModelSpec", ["model_dim", "word_embedding_dim",
