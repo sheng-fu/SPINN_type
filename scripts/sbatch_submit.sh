34bbb89c98fd71948b37239afa3d7efde075aa46
Sam Bowman <sbowman@stanford.edu>
2017-02-13 14:01:35 -0500

    SLURM submission script

---
+++ scripts/sbatch_submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Sets up a series of 10 jobs to run one after the other to get around time limits.
+
+
+bat=train_spinn.sbatch
+
+jobID=
+for((i=0; i<10; i++)); do
+    if [ "$jobID" == "" ]; then
+        jobID=$(sbatch $bat | awk '{print $NF}')
+    else
+        jobID=$(sbatch --dependency=afterany:$jobID $bat | awk '{print $NF}')
+    fi
+done
