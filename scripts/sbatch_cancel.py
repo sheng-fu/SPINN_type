9a09ae837787fbd4a542ad96e216656a16f41143
Sam Bowman <sbowman@stanford.edu>
2017-02-19 19:47:54 -0500

    Cancel script

--- scripts/sbatch_cancel.py
+++ scripts/sbatch_cancel.py
@@ -8,11 +8,12 @@ import getpass
 CURRENT_JOB = sys.argv[1]
 USER = getpass.getuser()
 	
-lines = subprocess.call(['squeue', '-u', USER, '-o', '"%.8A %.4C %.10m %.20E"'])
+lines = subprocess.check_output(['squeue', '-u', USER, '-o', '"%.8A %.4C %.10m %.20E"'])
+lines = lines.split('\n')
+lines.sort()
 
 to_kill = [CURRENT_JOB]
 
-lines.sort()
 for line in lines:
 	s = line.split()
 	if to_kill[-1] in s[1]:
