40d455288847dfdabf76c12a42cf7c8bcbb69a2c
Sam Bowman <sbowman@stanford.edu>
2017-02-19 19:44:06 -0500

    Infer username

--- scripts/sbatch_cancel.py
+++ scripts/sbatch_cancel.py
@@ -1,12 +1,12 @@
 import subprocess
 import sys
+import getpass
 
 ### Kill a job and its chain of dependents (as created by sbatch_submit).
-### Usage: python sbatch_cancel.py [Username] [Name of first running job in chain]
+### Usage: python sbatch_cancel.py [Name of first running job in chain]
 
-USER = sys.argv[1]
-CURRENT_JOB = sys.argv[2]
-
+CURRENT_JOB = sys.argv[1]
+USER = getpass.getuser()
 	
 lines = subprocess.call(['squeue', '-u', USER, '-o', '"%.8A %.4C %.10m %.20E"'])
 
