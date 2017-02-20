4ca219f22496770e7932c98fd330cae25ad92b38
Sam Bowman <sbowman@stanford.edu>
2017-02-20 15:24:03 -0500

    SLURM CPU-only scripts.

--- scripts/sbatch_submit.sh
+++ scripts/sbatch_submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Sets up a series of 10 jobs to run one after the other to get around time limits.
+# Optionally specify an sbatch script to use.
 
-
-bat=../scripts/train_spinn.sbatch
+bat=${1:-../scripts/train_spinn.sbatch}
 
 jobID=
 for((i=0; i<7; i++)); do
