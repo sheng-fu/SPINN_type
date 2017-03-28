e8183f3983521ee8d0e461233edd21b47c21fdaf
Andrew Drozdov <andrew@mrdrozdov.com>
2017-03-28 09:13:59 -0400

    Print train/eval samples together.

--- python/spinn/models/supervised_classifier.py
+++ python/spinn/models/supervised_classifier.py
@@ -16,6 +16,7 @@ from spinn.util.data import SimpleProgressBar
 from spinn.util.blocks import the_gpu, to_gpu, l2_cost, flatten, debug_gradient
 from spinn.util.misc import Accumulator, MetricsLogger, EvalReporter, time_per_token
 from spinn.util.misc import recursively_set_device
+from spinn.util.metrics import MetricsWriter
 from spinn.util.logging import train_format, train_extra_format, train_stats, train_accumulate
 from spinn.util.logging import eval_format, eval_extra_format
 from spinn.util.loss import auxiliary_loss
@@ -29,12 +30,309 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-from spinn.models.base import get_data_manager
-from spinn.models.base import main_loop, get_flags, flag_defaults, init_model
-from spinn.models.base import sequential_only, get_checkpoint_path, evaluate
+from spinn.models.base import get_data_manager, get_flags, get_batch
+from spinn.models.base import flag_defaults, init_model
+from spinn.models.base import sequential_only, get_checkpoint_path
 
 
 FLAGS = gflags.FLAGS
+
+
+def evaluate(model, eval_set, logger, step, vocabulary=None):
+    filename, dataset = eval_set
+
+    reporter = EvalReporter()
+
+    eval_str = eval_format(model)
+    eval_extra_str = eval_extra_format(model)
+
+    # Evaluate
+    class_correct = 0
+    class_total = 0
+    total_batches = len(dataset)
+    progress_bar = SimpleProgressBar(msg="Run Eval", bar_length=60, enabled=FLAGS.show_progress_bar)
+    progress_bar.step(0, total=total_batches)
+    total_tokens = 0
+    invalid = 0
+    start = time.time()
+
+    model.eval()
+
+    transition_preds = []
+    transition_targets = []
+
+    for i, batch in enumerate(dataset):
+        eval_X_batch, eval_transitions_batch, eval_y_batch, eval_num_transitions_batch, spans, eval_ids = get_batch(batch)
+
+        # Run model.
+        output = model(eval_X_batch, eval_transitions_batch, eval_y_batch,
+            use_internal_parser=FLAGS.use_internal_parser,
+            validate_transitions=FLAGS.validate_transitions)
+
+        # Normalize output.
+        logits = F.log_softmax(output)
+
+        # Calculate class accuracy.
+        target = torch.from_numpy(eval_y_batch).long()
+        pred = logits.data.max(1)[1].cpu() # get the index of the max log-probability
+        class_correct += pred.eq(target).sum()
+        class_total += target.size(0)
+
+        # Optionally calculate transition loss/acc.
+        transition_loss = model.transition_loss if hasattr(model, 'transition_loss') else None
+
+        # Update Aggregate Accuracies
+        total_tokens += sum([(nt+1)/2 for nt in eval_num_transitions_batch.reshape(-1)])
+
+        # Track number of examples with completely valid transitions.
+        if hasattr(model, 'spinn') and hasattr(model.spinn, 'invalid'):
+            num_sentences = 2 if model.use_sentence_pair else 1
+            invalid += model.spinn.invalid
+
+        # Accumulate stats for transition accuracy.
+        if transition_loss is not None:
+            transition_preds.append([m["t_preds"] for m in model.spinn.memories if m.get('t_preds', None) is not None])
+            transition_targets.append([m["t_given"] for m in model.spinn.memories if m.get('t_given', None) is not None])
+
+        if FLAGS.write_eval_report:
+            reporter_args = [pred, target, eval_ids, output.data.cpu().numpy()]
+            if hasattr(model, 'transition_loss'):
+                transitions_per_example = model.spinn.get_transitions_per_example(
+                    style="preds" if FLAGS.eval_report_use_preds else "given")
+                if model.use_sentence_pair:
+                    batch_size = pred.size(0)
+                    sent1_transitions = transitions_per_example[:batch_size]
+                    sent2_transitions = transitions_per_example[batch_size:]
+                    reporter_args.append(sent1_transitions)
+                    reporter_args.append(sent2_transitions)
+                else:
+                    reporter_args.append(transitions_per_example)
+            reporter.save_batch(*reporter_args)
+
+        # Print Progress
+        progress_bar.step(i+1, total=total_batches)
+    progress_bar.finish()
+
+    end = time.time()
+    total_time = end - start
+
+    # Get time per token.
+    time_metric = time_per_token([total_tokens], [total_time])
+
+    # Get class accuracy.
+    eval_class_acc = class_correct / float(class_total)
+
+    # Get transition accuracy if applicable.
+    if len(transition_preds) > 0:
+        all_preds = np.array(flatten(transition_preds))
+        all_truth = np.array(flatten(transition_targets))
+        eval_trans_acc = (all_preds == all_truth).sum() / float(all_truth.shape[0])
+    else:
+        eval_trans_acc = 0.0
+
+    has_invalid = hasattr(model, 'spinn') and hasattr(model.spinn, 'invalid')
+
+    stats_args = dict(
+        step=step,
+        class_acc=eval_class_acc,
+        transition_acc=eval_trans_acc,
+        filename=filename,
+        time=time_metric,
+        inv=invalid/float(total_batches) if has_invalid else 0.0,
+        )
+
+    logger.Log(eval_str.format(**stats_args))
+    logger.Log(eval_extra_str.format(**stats_args))
+
+    if FLAGS.write_eval_report:
+        eval_report_path = os.path.join(FLAGS.log_path, FLAGS.experiment_name + ".report")
+        reporter.write_report(eval_report_path)
+
+    return eval_class_acc, eval_trans_acc
+
+
+def train_loop(FLAGS, model, optimizer, trainer, training_data_iter, eval_iterators, logger, step, best_dev_error):
+    # Accumulate useful statistics.
+    A = Accumulator(maxlen=FLAGS.deque_length)
+    M = MetricsWriter(os.path.join(FLAGS.metrics_path, FLAGS.experiment_name))
+
+    # Checkpoint paths.
+    standard_checkpoint_path = get_checkpoint_path(FLAGS.ckpt_path, FLAGS.experiment_name)
+    best_checkpoint_path = get_checkpoint_path(FLAGS.ckpt_path, FLAGS.experiment_name, best=True)
+
+    # Build log format strings.
+    model.train()
+    X_batch, transitions_batch, y_batch, num_transitions_batch, spans, train_ids = get_batch(training_data_iter.next())
+    model(X_batch, transitions_batch, y_batch,
+            use_internal_parser=FLAGS.use_internal_parser,
+            validate_transitions=FLAGS.validate_transitions
+            )
+
+    logger.Log("")
+    logger.Log("# ----- BEGIN: Log Configuration ----- #")
+
+    # Preview train string template.
+    train_str = train_format(model)
+    logger.Log("Train-Format: {}".format(train_str))
+    train_extra_str = train_extra_format(model)
+    logger.Log("Train-Extra-Format: {}".format(train_extra_str))
+
+    # Preview eval string template.
+    eval_str = eval_format(model)
+    logger.Log("Eval-Format: {}".format(eval_str))
+    eval_extra_str = eval_extra_format(model)
+    logger.Log("Eval-Extra-Format: {}".format(eval_extra_str))
+
+    logger.Log("# ----- END: Log Configuration ----- #")
+    logger.Log("")
+
+    # Train.
+    logger.Log("Training.")
+
+    # New Training Loop
+    progress_bar = SimpleProgressBar(msg="Training", bar_length=60, enabled=FLAGS.show_progress_bar)
+    progress_bar.step(i=0, total=FLAGS.statistics_interval_steps)
+
+    for step in range(step, FLAGS.training_steps):
+        model.train()
+
+        start = time.time()
+
+        batch = get_batch(training_data_iter.next())
+        X_batch, transitions_batch, y_batch, num_transitions_batch, spans, train_ids = batch
+
+        total_tokens = sum([(nt+1)/2 for nt in num_transitions_batch.reshape(-1)])
+
+        # Reset cached gradients.
+        optimizer.zero_grad()
+
+        if FLAGS.model_type == "RLSPINN":
+            model.spinn.epsilon = FLAGS.rl_epsilon * math.exp(-step/FLAGS.rl_epsilon_decay)
+
+        # Run model.
+        output = model(X_batch, transitions_batch, y_batch,
+            use_internal_parser=FLAGS.use_internal_parser,
+            validate_transitions=FLAGS.validate_transitions
+            )
+
+        # Normalize output.
+        logits = F.log_softmax(output)
+
+        # Calculate class accuracy.
+        target = torch.from_numpy(y_batch).long()
+        pred = logits.data.max(1)[1].cpu() # get the index of the max log-probability
+        class_acc = pred.eq(target).sum() / float(target.size(0))
+
+        # Calculate class loss.
+        xent_loss = nn.NLLLoss()(logits, to_gpu(Variable(target, volatile=False)))
+
+        # Optionally calculate transition loss.
+        transition_loss = model.transition_loss if hasattr(model, 'transition_loss') else None
+
+        # Extract L2 Cost
+        l2_loss = l2_cost(model, FLAGS.l2_lambda) if FLAGS.use_l2_cost else None
+
+        # Accumulate Total Loss Variable
+        total_loss = 0.0
+        total_loss += xent_loss
+        if l2_loss is not None:
+            total_loss += l2_loss
+        if transition_loss is not None and model.optimize_transition_loss:
+            total_loss += transition_loss
+        total_loss += auxiliary_loss(model)
+
+        # Backward pass.
+        total_loss.backward()
+
+        # Hard Gradient Clipping
+        clip = FLAGS.clipping_max_value
+        for p in model.parameters():
+            if p.requires_grad:
+                p.grad.data.clamp_(min=-clip, max=clip)
+
+        # Learning Rate Decay
+        if FLAGS.actively_decay_learning_rate:
+            optimizer.lr = FLAGS.learning_rate * (FLAGS.learning_rate_decay_per_10k_steps ** (step / 10000.0))
+
+        # Gradient descent step.
+        optimizer.step()
+
+        end = time.time()
+
+        total_time = end - start
+
+        train_accumulate(model, A, batch)
+        A.add('class_acc', class_acc)
+        A.add('total_tokens', total_tokens)
+        A.add('total_time', total_time)
+
+        if step % FLAGS.statistics_interval_steps == 0:
+            progress_bar.step(i=FLAGS.statistics_interval_steps, total=FLAGS.statistics_interval_steps)
+            progress_bar.finish()
+
+            A.add('xent_cost', xent_loss.data[0])
+            A.add('l2_cost', l2_loss.data[0])
+            stats_args = train_stats(model, optimizer, A, step)
+
+            metric_stats = ['class_acc', 'total_cost', 'transition_acc', 'transition_cost']
+            for key in metric_stats:
+                M.write(key, stats_args[key], step)
+
+            logger.Log(train_str.format(**stats_args))
+            logger.Log(train_extra_str.format(**stats_args))
+
+        if step % FLAGS.sample_interval_steps == 0 and FLAGS.num_samples > 0:
+            model.train()
+            model(X_batch, transitions_batch, y_batch,
+                use_internal_parser=FLAGS.use_internal_parser,
+                validate_transitions=FLAGS.validate_transitions
+                )
+            tr_transitions_per_example = model.spinn.get_transitions_per_example()
+
+            model.eval()
+            model(X_batch, transitions_batch, y_batch,
+                use_internal_parser=FLAGS.use_internal_parser,
+                validate_transitions=FLAGS.validate_transitions
+                )
+            ev_transitions_per_example = model.spinn.get_transitions_per_example()
+
+            transition_str = "Samples:"
+            if model.use_sentence_pair and len(transitions_batch.shape) == 3:
+                _transitions_batch = np.concatenate([
+                    transitions_batch[:,:,0], transitions_batch[:,:,1]], axis=0)
+
+            # This could be done prior to running the batch for a tiny speed boost.
+            t_idxs = range(FLAGS.num_samples)
+            random.shuffle(t_idxs)
+            t_idxs = sorted(t_idxs[:FLAGS.num_samples])
+            for t_idx in t_idxs:
+                gold = _transitions_batch[t_idx]
+                pred_tr = tr_transitions_per_example[t_idx]
+                pred_ev = ev_transitions_per_example[t_idx]
+                _, crossing = evalb.crossing(gold, pred)
+                transition_str += "\n{}. crossing={}".format(t_idx, crossing)
+                transition_str += "\n     g{}".format("".join(map(str, gold)))
+                transition_str += "\n    pt{}".format("".join(map(str, pred_tr)))
+                transition_str += "\n    pe{}".format("".join(map(str, pred_ev)))
+            logger.Log(transition_str)
+
+        if step > 0 and step % FLAGS.eval_interval_steps == 0:
+            for index, eval_set in enumerate(eval_iterators):
+                acc, tacc = evaluate(model, eval_set, logger, step)
+                if FLAGS.ckpt_on_best_dev_error and index == 0 and (1 - acc) < 0.99 * best_dev_error and step > FLAGS.ckpt_step:
+                    best_dev_error = 1 - acc
+                    logger.Log("Checkpointing with new best dev accuracy of %f" % acc)
+                    trainer.save(best_checkpoint_path, step, best_dev_error)
+                if index == 0:
+                    M.write('eval_class_acc', acc, step)
+                    M.write('eval_transition_acc', tacc, step)
+            progress_bar.reset()
+
+        if step > FLAGS.ckpt_step and step % FLAGS.ckpt_interval_steps == 0:
+            logger.Log("Checkpointing.")
+            trainer.save(standard_checkpoint_path, step, best_dev_error)
+
+        progress_bar.step(i=step % FLAGS.statistics_interval_steps, total=FLAGS.statistics_interval_steps)
 
 
 def run(only_forward=False):
@@ -147,7 +445,7 @@ def run(only_forward=False):
         for index, eval_set in enumerate(eval_iterators):
             acc = evaluate(model, eval_set, logger, step, vocabulary)
     else:
-        main_loop(FLAGS, model, optimizer, trainer, training_data_iter, eval_iterators, logger, step, best_dev_error)
+        train_loop(FLAGS, model, optimizer, trainer, training_data_iter, eval_iterators, logger, step, best_dev_error)
 
 
 if __name__ == '__main__':
