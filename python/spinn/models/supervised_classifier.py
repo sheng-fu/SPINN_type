2e906f2b865c1a67fa7c6ce6281d44a0d6782f56
Andrew Drozdov <andrew@mrdrozdov.com>
2017-04-02 17:34:54 -0400

    Restrict RL to reinforce.py.

--- python/spinn/models/supervised_classifier.py
+++ python/spinn/models/supervised_classifier.py
@@ -450,4 +450,7 @@ if __name__ == '__main__':
 
     flag_defaults(FLAGS)
 
+    if FLAGS.model_type == "RLSPINN":
+        raise Exception("Please use reinforce.py instead of supervised_classifier.py for RLSPINN.")
+
     run(only_forward=FLAGS.expanded_eval_only_mode)
