4ee6f8569240cc553518fb600a09e534b1875865
Sam Bowman <sbowman@stanford.edu>
2017-05-22 17:38:05 -0400

    Turn off 'levels' feature in logger by default.

--- python/spinn/util/afs_safe_logger.py
+++ python/spinn/util/afs_safe_logger.py
@@ -12,7 +12,7 @@ class Logger(object):
     WARNING = 2
     ERROR = 3
 
-    def __init__(self, log_path=None, json_log_path=None, min_print_level=0, min_file_level=0):
+    def __init__(self, log_path=None, json_log_path=None, show_level=False, min_print_level=0, min_file_level=0):
         # log_path: The full path for the log file to write. The file will be appended
         #   to if it exists.
         # min_print_level: Only messages with level above this level will be printed to stderr.
@@ -22,6 +22,7 @@ class Logger(object):
         self.json_log_path = json_log_path
         self.min_print_level = min_print_level
         self.min_file_level = min_file_level
+        self.show_level = show_level
 
     def Log(self, message, level=INFO):
         if level >= self.min_print_level:
@@ -32,7 +33,10 @@ class Logger(object):
             with open(self.log_path, 'a') as f:
                 datetime_string = datetime.datetime.now().strftime(
                     "%y-%m-%d %H:%M:%S")
-                f.write("%s [%i] %s\n" % (datetime_string, level, message))
+                if self.show_level:
+                    f.write("%s [%i] %s\n" % (datetime_string, level, message))
+                else:
+                    f.write("%s %s\n" % (datetime_string, message))                    
 
     def LogJSON(self, message_obj, level=INFO):
         if self.json_log_path and level >= self.min_file_level:
