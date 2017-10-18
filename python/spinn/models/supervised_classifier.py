ac074fca0c63bdf5c1f2b658472e3e988fe415c5
woollysocks <nikitanangia@gmail.com>
2017-10-18 17:42:41 -0400

    maillard options

--- python/spinn/models/supervised_classifier.py
+++ python/spinn/models/supervised_classifier.py
@@ -83,7 +83,7 @@ def evaluate(FLAGS, model, eval_set, log_entry,
             example_lengths=eval_num_transitions_batch)
 
         # TODO: Restore support in Pyramid if using.
-        can_sample = FLAGS.model_type in ["ChoiPyramid"] or (
+        can_sample = FLAGS.model_type in ["ChoiPyramid", "Maillard"] or (
             FLAGS.model_type == "SPINN" and FLAGS.use_internal_parser)
         if show_sample and can_sample:
             tmp_samples = model.get_samples(
