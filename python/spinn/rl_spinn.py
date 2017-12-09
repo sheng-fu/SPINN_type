More softmax API fixes.

python/spinn/rl_spinn.py
@@ -68,7 +68,7 @@ class RLSPINN(SPINN):
 
     def predict_actions(self, transition_output):
         transition_output_t = transition_output / max(self.temperature, TINY)
-        transition_dist = F.softmax(transition_output_t)
+        transition_dist = F.softmax(transition_output_t, dim=1)
 
         if self.catalan:
             # Use the catalan distribution as a prior.
@@ -87,7 +87,7 @@ class RLSPINN(SPINN):
         if self.catalan and self.catalan_backprop:
             transition_logdist = torch.log(transition_dist + TINY)
         else:
-            transition_logdist = F.log_softmax(transition_output_t)
+            transition_logdist = F.log_softmax(transition_output_t, dim=1)
         shift_probs = transition_dist.data[:, 0]
 
         if self.training:
@@ -224,7 +224,7 @@ class BaseModel(_BaseModel):
             output = self.run_greedy(sentences, transitions)
 
             # Estimate Reward
-            probs = F.softmax(output).data.cpu()
+            probs = F.softmax(output, dim=1).data.cpu()
             target = torch.from_numpy(y_batch).long()
             approx_rewards = self.build_reward(
                 probs, target, rl_reward=self.rl_reward)
@@ -321,7 +321,7 @@ class BaseModel(_BaseModel):
         if not self.training:
             return
 
-        probs = F.softmax(output).data.cpu()
+        probs = F.softmax(output, dim=1).data.cpu()
         target = torch.from_numpy(y_batch).long()
 
         # Get Reward.